fe3c0cd31d8726f6683eb879f4be75902c518c58
jsanjuan <jessica.sanjuan@ucalgary.ca>
2025-03-05 12:17:58 -0700

    Finished merge
    Fully implemented merge

--- ex2.py
+++ ex2.py
@@ -41,4 +41,15 @@ class PriorityQueueSorted:
 
         while (i < len(left) and j < len(right)):
             if(left[i] < right[j]):
-                pass
+                result.append(left[i])
+                i += 1
+            else:
+                result.append(right[j])
+                j += 1
+
+        # append elements 
+        result.extend(left[i:])
+        result.extend(right[j:])
+        
+        return result
+
