f33678ad715e1bc85771f01a5d95abe59b3bf0a7
jsanjuan <jessica.sanjuan@ucalgary.ca>
2025-03-08 14:31:12 -0700

    ex3 implement stack
    Implementation of stack that uses internal python array .push() and .pop()

--- ex3.py
+++ ex3.py
@@ -1,3 +1,26 @@
+import random
+import timeit
+import matplotlib.pyplot as plt
 
 
-# Q1
+# Q1 Implementation of stack that uses internal python .push() and .pop()
+class ArrayStack:
+    def __init__(self):
+        self.stack = []
+    
+    def push(self, value):
+        self.stack.append(value)
+    
+    def pop(self):
+        if self.stack:
+            return self.stack.pop()
+        return None
+
+
+# Q2
+
+# Q3 
+
+# Q4 
+
+# Q5
