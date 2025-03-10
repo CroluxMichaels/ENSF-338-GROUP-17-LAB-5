acb174d6d92d4cc79146e09a475b7e6d53c74c28
Crolu <chiruliacosta@gmail.com>
2025-03-10 10:07:20 -0600

    Finished ex1

--- ex1.py
+++ ex1.py
@@ -3,37 +3,71 @@ import sys
 # Stack Implementation taken from course GitHub
 
 class MyArrayStack:
+
     def __init__(self):
         self._storage = []
+
     def push(self, value):
         self._storage.append(value)
+
     def pop(self):
         # Note that in Python "not <list>" evaluates to True if the list is empty
         if not self._storage:
             return None
         else:
             return self._storage.pop()
+        
     def peek(self):
         if not self._storage:
             return None
         else:
             return self._storage[-1]
-    def getlen(self):
-        return len(self._storage)
-    def reverse(self):
-        self._storage.reverse()
+
+def evaluate(stack):
+    element = stack.pop()
+
+    if element == '(':
+        operator = stack.pop()
+        items = []
+
+        while stack.peek() != ')':
+            items.append(evaluate(stack))
+
+        stack.pop()
+
+        if operator == '+':
+            return sum(items)
+        elif operator == '-':
+            return items[0] - sum(items[1:])
+        elif operator == '*':
+            result = items[0]
+            for i in items[1:]:
+                result *= i
+            return result
+        elif operator == '/':
+            result = items[0]
+            for i in items[1:]:
+                result /= i
+            return result
+    else:
+        return float(element)
+    
 
 def main(expression):
-    print(f"Argument 1: {expression}")
+    
     stack = MyArrayStack()
-    for i in expression:
+    list = expression.replace('(', ' ( ').replace(')', ' ) ').split()
+    list.reverse()
+
+    for i in list:
         stack.push(i)
-    stack.reverse()
-    while stack.getlen() > 0:
-        print(stack.pop())
+
+    result = evaluate(stack)
+    print(result)
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         main(sys.argv[1])
     else:
-        print("No expression provided.")        
+        print("No expression provided.")
+        print("Please enter an appropriate expression as a command-line argument when running the program.")        
